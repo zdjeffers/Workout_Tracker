7bd43a755998299b5c6e38c16bf4635d8158ff9f
Dave Jeffers <72349891+zdjeffers@users.noreply.github.com>
2021-04-07 18:04:31 -0400

    added .gitignore file. Updated routes folder

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require('morgan');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(logger('dev'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./Routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`Workout Tracker is running on port ${PORT}!`);
+});
